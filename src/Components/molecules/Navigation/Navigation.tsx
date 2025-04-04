5ac7538ca515879a36fb2fff02ef56ccfa8dbc26
N.Z. Timisela <noatimisela@gmail.com>
2025-04-04 21:02:50 +0200

    Add InputField to Navigation

--- src/Components/molecules/Navigation/Navigation.tsx
+++ src/Components/molecules/Navigation/Navigation.tsx
@@ -1,8 +1,10 @@
 import Button from "../../atoms/Button/Button";
+import InputField from "../../atoms/InputField/InputField";
 
 const Navigation = () => {
     return (
         <>
+            <InputField placeholder={"Item"} type={"text"} />
             <Button label={"Add"} disabled={false} />
         </>
     );
