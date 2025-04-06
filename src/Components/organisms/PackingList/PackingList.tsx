265163381c5490b4a8d22c3cdc822edccd968efb
N.Z. Timisela <noatimisela@gmail.com>
2025-04-06 15:22:53 +0200

    Add: The actual PackingList component..

---
+++ src/Components/organisms/PackingList/PackingList.tsx
@@ -0,0 +1,11 @@
+import Form from "../../molecules/Form/Form";
+
+const PackingList = (): React.JSX.Element => {
+    return (
+        <>
+            <Form />
+        </>
+    );
+};
+
+export default PackingList;
