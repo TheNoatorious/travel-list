44203ef09aca40381e4c2d504e8283f13ca7ddc0
N.Z. Timisela <noatimisela@gmail.com>
2025-04-06 15:21:51 +0200

    Fix: Remove Navigation component

--- src/Components/organisms/Header/Header.tsx
+++ src/Components/organisms/Header/Header.tsx
@@ -1,12 +1,10 @@
 import "./Header.css";
 import Hero from "../../molecules/Hero/Hero";
-import Navigation from "../../molecules/Navigation/Navigation";
 
 const Header = (): React.JSX.Element => {
     return (
         <header className="header">
             <Hero />
-            <Navigation />
         </header>
     );
 };
